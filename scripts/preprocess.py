9439f127f1ad9a4fc1b9095e558c912b2e22763a
Vineeth Krishnankutty <vineeth.k2010@gmail.com>
2025-03-10 09:52:15 +0000

    Initial Commit - Project files and requirements

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -4,3 +4,12 @@ import numpy as np
 from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
+
+
+def preprocess(text):
+    text = re.sub(r"[^a-zA-Z\s]", " ", text)
+    text = text.lower().strip()
+    tokens = word_tokenize(text)
+    stop_words = set(stopwords.words("english"))
+
+
